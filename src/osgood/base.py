70a685dfc52c5b42725ce9bb6b851035781a3b46
charlesriver456 <none>
2023-02-19 15:25:48 -0500

    fixed precommit

--- src/osgood/base.py
+++ src/osgood/base.py
@@ -1,6 +1,6 @@
-#A collection of useful functions
+# A collection of useful functions
 # 1
 
-def ozip(list_to_zip: list, groups: int) -> list:
-		return list(zip(*[iter(list_to_zip)] * groups))
 
+def ozip(list_to_zip: list, groups: int) -> list:
+    return list(zip(*[iter(list_to_zip)] * groups))
