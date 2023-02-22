088cc8a4bf9191f806f08c99e47482a2e84a3516
Damian Barabonkov <damianb@mit.edu>
2023-02-22 21:50:56 +0100

    Implemented 'pad' parameter to 'ozip' to group any straggling elements with 'None'

--- src/osgood/base.py
+++ src/osgood/base.py
@@ -1,13 +1,11 @@
 # A collection of useful functions
 import warnings
+from itertools import repeat, chain
 from typing import Any
 
 
-def ozip(list_to_zip: list[Any], groups: int) -> list[tuple]:
+def ozip(list_to_zip: list[Any], groups: int, pad: bool = False) -> list[tuple]:
     """Zips adjacent elements of list ``list_to_zip`` into groupings of a given size.
-
-    If ``len(list_to_zip) % groups != 0``, the remaining elements are ommitted.
-    If ``groups > len(list_to_zip)``, then an empty list is returned.
 
     Parameters
     ----------
@@ -15,17 +13,26 @@ def ozip(list_to_zip: list[Any], groups: int) -> list[tuple]:
         The input list whose elements to group-wise zip.
     group
         The size of each group in the resulting zip.
+    pad
+        A flag whether to pad with ``None`` any ungrouped straggling 
+        elements of ``list_to_zip`` to form one final group. When left
+        as `False`, then if ``len(list_to_zip) % groups != 0``, 
+        the remaining elements are ommitted.
 
     Returns
     -------
     list[tuple]
         The resulting list of groupings as tuples.
     """
-    if len(list_to_zip) % groups != 0:
-        remainder = len(list_to_zip) % groups
+    pad_len = groups - len(list_to_zip) % groups
+    
+    if not pad and pad_len != 0:
+        n_trim = groups - pad_len
         warnings.warn(
-            f"This part of passed list will be shaved off: {list_to_zip[-1 * remainder:]}"
+            f"This part of passed list will be shaved off: {list_to_zip[-1 * n_trim:]}"
         )
-
-    return list(zip(*[iter(list_to_zip)] * groups))
+    
+    # Pad with `None` if requested
+    iterator = chain(list_to_zip, repeat(None, pad_len if pad else 0))
+    return list(zip(*[iter(iterator)] * groups))
 
