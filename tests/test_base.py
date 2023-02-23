f42cebfa1c520704d3559acd4f3a0e4a479447d7
charlesriver456 <none>
2023-02-22 21:26:49 -0500

    fix reqs and testing

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,24 +1,11 @@
 import osgood
 from osgood.base import ozip
-import typing
+import pytest
 
 
-def test_ozip_1():
-    example_list = [1, 2, 3, 4]
-    example_group = 4
-    response = ozip(example_list, example_group)
-    assert response == [(1, 2, 3, 4)]
-
-
-def test_ozip_2():
-    example_list = [1, 2, 3]
-    example_group = 4
-    response = ozip(example_list, example_group)
-    assert response == []
-
-
-def test_ozip_3():
-    example_list = [1, 2, 3]
-    example_group = 2
-    response = ozip(example_list, example_group)
-    assert response == [(1, 2)]
+@pytest.mark.parametrize(
+    "test_list, passed_int, expected",
+    [([1, 2, 3, 4], 4, [(1, 2, 3, 4)]), ([1, 2, 3], 4, []), ([1, 2, 3], 2, [(1, 2)])],
+)
+def test_ozip_1(test_list, passed_int, expected):
+    assert ozip(test_list, passed_int) == expected
