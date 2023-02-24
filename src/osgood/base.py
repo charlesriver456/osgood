61d368d51b4221ce34f5b139e03befb1fa34faa7
charlesriver456 <none>
2023-02-24 13:57:29 -0500

    fix

--- src/osgood/base.py
+++ src/osgood/base.py
@@ -5,37 +5,36 @@ from functools import wraps
 from typing import List, Tuple, Optional, Callable
 
 
+def timeit(fn_identifier: Optional[str] = None) -> Callable:
+    """Decorates a function such that its execution time is printed.
+    Parameters
+    ----------
+    fn_identifier
+        An optional string to print along with the execution time to make
+        the association simpler between function and time. Defaults to
+        the name of the decorated function.
+    Returns
+    -------
+    Callable
+        The decorated function that will track the execution time.
+    """
 
+    def decorator(func: Callable):
+        # Default to the name of the function if no identifier is supplied
+        fn_identifier = func.__name__ if fn_identifier is None else fn_identifier
 
+        @wraps(func)
+        def inner(*args: Any, **kwargs: Any) -> Any:
+            start = time.perf_counter()
+            result = func(*args, **kwargs)
+            end = time.perf_counter()
 
+            print(f"Elapsed time ({fn_identifier}): {end - start}")
+            return result
 
+        return inner
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return decorator
 
 
 def ozip(list_to_zip: list[Any], groups: int, pad: bool = False) -> list[tuple]:
